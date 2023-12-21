18f018551c814017077b70975bf108e9839c042a
stanleywanjau <stanleywanjau371@gmail.com>
2023-12-21 04:38:43 +0300

    completed

---
+++ app/migrations/versions/4efbc4b70f10_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 4efbc4b70f10
+Revises: 
+Create Date: 2023-12-21 04:09:24.742627
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '4efbc4b70f10'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
